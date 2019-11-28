6c43f036123db8cf7760b1a1fc336417d890f921
Peter Züger <zueger.peter@icloud.com>
2019-11-28 15:57:56 +0100

    catch KeyError in getattr.py and print None

--- tools/getattr.py
+++ tools/getattr.py
@@ -2,13 +2,19 @@
 import sys
 import json
 
-if __name__ == '__main__':
+def main():
     with open(sys.argv[1]) as f:
         j = json.loads(f.read())
         args = sys.argv[2:]
         for i in args:
-            if isinstance(j, list):
-                j = j[int(i)]
-            else:
-                j = j[i]
-        print(j)
+            try:
+                if isinstance(j, list):
+                    j = j[int(i)]
+                else:
+                    j = j[i]
+            except KeyError:
+                return None
+        return j
+
+if __name__ == '__main__':
+    print(main())
